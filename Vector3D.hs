4eedf14b5a4039bf968e207f4d5ed5b5b2cac29a
HIRAI, Hajime <hirai.hajime@gmail.com>
2012-04-12 00:11:32 +0900

    remove some functions

--- Vector3D.hs
+++ Vector3D.hs
@@ -16,7 +16,7 @@ instance Num Vector3D where
     (Vec a1 a2 a3) - (Vec b1 b2 b3) = Vec (a1 - b1) (a2 - b2) (a3 - b3)
     (Vec a1 a2 a3) * (Vec b1 b2 b3) =
         Vec (a2*b3 - a3*b2) (a3*b1 - b1*b3) (a1*b2 - a2*b1)
-    negate (Vec a1 a2 a3) = Vec (negate a1) (negate a2) (negate a3)
-    abs (Vec a1 a2 a3) = Vec (abs a1) (abs a2) (abs a3)
+    negate (Vec a1 a2 a3) = undefined
+    abs (Vec a1 a2 a3) = undefined
     signum v@(Vec x y z) = let d = mag v in Vec (x / d) (y / d) (z / d)
-    fromInteger n = Vec (fromInteger n) 0 0
+    fromInteger n = undefined
