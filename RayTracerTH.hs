65762d84be6b79bf92859127a7e80b09d581ad4b
HIRAI, Hajime <hirai.hajime@gmail.com>
2012-05-17 21:14:49 +0900

    output rendered bitmap using runIO

--- RayTracerTH.hs
+++ RayTracerTH.hs
@@ -1,22 +1,17 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module RayTracerTH where
 
 import Language.Haskell.TH
 
-import RayTracer hiding (trace, packCol)
-
-import Data.Word (Word8)
+import RayTracer
 import Vector3D
 
-packCol :: Color -> [Word8]
-packCol (Color r g b) = [r, g, b, 0]
+import Codec.BMP
 
-trace :: World -> Vector3D ->
-         Float -> Float -> Float -> Float -> Float ->
-         ExpQ
-trace w eyePos startx endx starty endy step = do
-  let ws = concat [packCol $ colorAt w eyePos col row |
-                   row <- [starty, starty + step .. endy],
-                   col <- [startx, startx + step .. endx]]
-  listE $ map (litE . integerL . fromIntegral) ws
+runTrace :: World -> Vector3D ->
+            Float -> Float -> Float -> Float -> Float ->
+            ExpQ
+runTrace w eyePos startx endx starty endy step = do
+  runIO $ writeBMP "result.bmp" $ trace w eyePos startx endx starty endy step
+  tupE []
--- ray_tracer_th.hs
+++ ray_tracer_th.hs
@@ -1,21 +1,9 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import RayTracerTH
 import RayTracer (world_b, world_c, eyePos)
-import Codec.BMP
-import Data.ByteString (pack)
 
 main :: IO ()
-main =
-  let startx = -96
-      endx = 96
-      starty = -54
-      endy = 54
-      step = 2.0
-      width = 1 + floor ((endx - startx) / step)
-      height = 1 + floor ((endy - starty) / step) in
-  writeBMP "result.bmp" $
-  packRGBA32ToBMP width height $
-  pack $ $(trace (world_b ++ world_c) eyePos (-96) 96 (-54) 54 2.0)
+main = return $(runTrace (world_b ++ world_c) eyePos (-96) 96 (-54) 54 1.0)
